e62852a60278422ed4bbafb6aaba518450c4fd87
Alfredo20164920 <apaz20164920@gmail.com>
2024-08-18 21:02:51 -0600

    Import and Show data

--- Program.cs
+++ Program.cs
@@ -1,2 +1,13 @@
 ﻿// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
+
+    LinqQueries queries = new LinqQueries();
+    printValues(queries.AllCollection());
+
+
+    void printValues (IEnumerable<Book> BookList) {
+        Console.WriteLine("{0, -60}, {1, 7}, {2, 11}\n  ", "Title", "# Pages", "Published Date");
+        foreach(var item in BookList) {
+            Console.WriteLine("{0, -60}, {1, 7}, {2, 11}", item.Title, item.PageCount, item.PublishedDate.ToShortDateString());
+        }
+    }
